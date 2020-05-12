c6a298929e6f3ebcabdfd632427dd3a5c4f92aa9
Nathan Peeters <nathanpeeters@MacBook-Air-de-Nathan.local>
2020-05-12 10:55:23 +0200

    CSS Responsive aux medias

--- controllers/matchapicontroller.js
+++ controllers/matchapicontroller.js
@@ -3,16 +3,17 @@ let Match = require('../models/matchmodel.js');
 listematchs = [];
 /* Affichage de tous les Matchs dans une API au format JSON */
 exports.matchs = function (req, res) {
-    connection.query(" SELECT * from Matchs INNER JOIN Coachs ON Matchs.fk_id_coach = Coachs.id ;", function (error, resultSQL3) {
-        if (error) {
-            res.status(400).json({ "message": 'error' });
-        }
+    connection.query
+        (" SELECT * from Matchs INNER JOIN Coachs ON Matchs.fk_id_coach = Coachs.id ;", function (error, resultSQL3) {
+            if (error) {
+                res.status(400).json({ "message": 'error' });
+            }
 
-        else {
-            res.status(200);
-            res.json({ listematchs: resultSQL3 });
-        }
-    })
+            else {
+                res.status(200);
+                res.json({ listematchs: resultSQL3 });
+            }
+        })
 };
 
 /* Selection d'un match (API) */
@@ -28,17 +29,23 @@ exports.getmatch = function (req, res) {
             res.status(200);
             console.log(resultSQL);
             matchs = resultSQL;
-            res.json({
-                id_match: matchs[0].id_match, titre: matchs[0].titre, date: matchs[0].date, heure: matchs[0].heure, adresse: matchs[0].adresse, fk_id_coach: matchs[0].fk_id_coach, AG: matchs[0].AG, BU: matchs[0].BU,
-                AD: matchs[0].AD, MG: matchs[0].MG, MC: matchs[0].MC, MD: matchs[0].MD, DG: matchs[0].DG, DCG: matchs[0].DCG, DCD: matchs[0].DCD, DD: matchs[0].DD, G: matchs[0].G
+            res.json({matchs : matchs
             });
         }
     });
 }
 /* On ajoute un élément à la liste des matchs */
 exports.addmatch = function (req, res) {
-    let match = new Match(req.body.id_match, req.body.titre, req.body.date, req.body.heure, req.body.adresse, req.body.fk_id_coach, req.body.AG, req.body.BU, req.body.AD,
-        req.body.MG, req.body.MC, req.body.MD, req.body.DG, req.body.DCG, req.body.DCD, req.body.DD, req.body.G);
+    let match = new Match(
+        req.body.id_match, req.body.titre, 
+        req.body.date, req.body.heure, 
+        req.body.adresse, req.body.fk_id_coach, 
+        req.body.AG, req.body.BU, req.body.AD,
+        req.body.MG, req.body.MC, 
+        req.body.MD, req.body.DG, 
+        req.body.DCG, req.body.DCD, 
+        req.body.DD, req.body.G
+        );
     console.log(match);
     connection.query("INSERT INTO Matchs set ?", match, function (error, resultSQL) {
         if (error) {
@@ -64,8 +71,14 @@ exports.supprmatch = function (req, res) {
 /* Update d'un match (route API) */
 
 exports.updatematch = function (req, res) {
-    let match = new Match(req.body.id_match, req.body.titre, req.body.date, req.body.heure, req.body.adresse, req.body.fk_id_coach, req.body.AG, req.body.BU, req.body.AD,
-        req.body.MG, req.body.MC, req.body.MD, req.body.DG, req.body.DCG, req.body.DCD, req.body.DD, req.body.G);
+    let match = new Match(
+        req.body.id_match, req.body.titre, 
+        req.body.date, req.body.heure, 
+        req.body.adresse, req.body.fk_id_coach, 
+        req.body.AG, req.body.BU, req.body.AD,
+        req.body.MG, req.body.MC, req.body.MD, 
+        req.body.DG, req.body.DCG, req.body.DCD, 
+        req.body.DD, req.body.G);
     console.log(match);
     connection.query("UPDATE Matchs SET ? WHERE id_match = ?",
         [match, req.body.id_match], function (error, resultSQL) {
